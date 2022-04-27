e5017bd480e88d18a78730316d447ac35cfcf7cd
Yau <yau@tractive.com.my>
2022-04-27 11:17:37 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar';
+import Create from './components/Create';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   // const title = 'Welcome to the new blog';
@@ -10,22 +12,37 @@ function App() {
   // reason of using "className" in html due to class is a reserved keyword in javascript
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    // All components inside the Router tag will be having access to the router
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* Switch tag ensure one route will be showing in one time
+          Navbar will be remained due to outside of Switch tag
+          Only components inside the Switch tag will be invoked */}
+          <Switch>
+            {/* Without exact property in Route tag, the route will check whether the path is included in any of the path
+            E.g. when the path is "/create", it will still redirect to Home page because "/" exists in "/create".
+            That's why we add exact property to make sure it matches full path */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+          {/* <h1>{title}</h1>
+          <p>Like {likes} times</p>
 
-        {/* <h1>{title}</h1>
-        <p>Like {likes} times</p>
+          input object will give error
+          e.g. {person} will give error since person is an object
+          <p>person: {person.name}</p>
 
-        input object will give error
-        e.g. {person} will give error since person is an object
-        <p>person: {person.name}</p>
-
-        all output will be converted into string
-        <p>{[1, 2, 3, 4, 5]}</p> */}
+          all output will be converted into string
+          <p>{[1, 2, 3, 4, 5]}</p> */}
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
